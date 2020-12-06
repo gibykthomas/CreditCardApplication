9c20a727a68bcabb451e563cb18ab88c26f90935
Giby Thomas <gibykthomas@gmail.com>
2020-12-06 14:55:02 +0000

    in memory db set up

---
+++ src/main/java/com/sapient/creditcardApplication/config/R2DBCConfiguration.java
@@ -0,0 +1,24 @@
+package com.sapient.creditcardApplication.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
+
+import io.r2dbc.spi.ConnectionFactory;
+
+@Configuration
+public class R2DBCConfiguration {
+
+    @Bean
+    ConnectionFactoryInitializer initializer(ConnectionFactory connectionFactory) {
+
+      ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
+      initializer.setConnectionFactory(connectionFactory);
+      initializer.setDatabasePopulator(new ResourceDatabasePopulator(new ClassPathResource("schema/schema.sql")));
+
+      return initializer;
+    }
+
+}
---
+++ src/main/resources/schema/schema.sql
@@ -0,0 +1,1 @@
+CREATE TABLE creditcard (number VARCHAR(20) PRIMARY KEY, name VARCHAR(255), balance DECIMAL, creditlimit DECIMAL);
